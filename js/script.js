eee3694027f8c8c519fdd4f3a2f5cb2d70aff676
Lazcisco <williamalexfoster@gmail.com>
2022-06-27 21:59:41 -0400

    pokemonList array
    created an array containing 3 pokemon represented by objects with 3 keys for identification

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let pokemonList = [
+    {
+        name: 'Haxorus',
+        height: 1.8,
+        types: 'Dragon'
+    },
+    {
+        name: 'Omastar',
+        height: 1,
+        types: ['Rock', 'Water']
+    },
+    {
+        name: 'Vanilluxe',
+        height: 1.3,
+        types: 'Ice'
+    }
+];
